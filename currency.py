3048f065be279a6bea62ca1e28013d38beeae99d
Kai Stullich <kairstullich@gmail.com>
2017-01-27 20:13:01 -0800

    removed if statement, and only loop through [:3] for currenct_table_row

--- currency.py
+++ currency.py
@@ -3,18 +3,14 @@ from urllib.request import urlopen
 
 url = 'http://www.x-rates.com/table/?from=USD&amount=1'
 page = urlopen(url)
-soup = BeautifulSoup(page, 'html.parser')
+soup = BeautifulSoup(page, 'lxml')
 
 currency_table = soup.find(class_='ratesTable')
-currency_table_row_name = currency_table.find_all('td')
+currency_table_row = currency_table.find_all('td')
 
 euro_currency = []
-for tag in currency_table_row_name:
+for tag in currency_table_row[:3]:
     euro_text = tag.get_text()  # pull only the text from the 'td' tags
     euro_append = euro_currency.append(euro_text)  # append the text from 'td' tags
-    if 'British Pound' in tag:  # stop when 'British Pounds' is found
-        break
-
-del euro_currency[-1]  # Delete 'British Pounds' from the list always last element
 
 print(euro_currency)
