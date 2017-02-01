ef57abfdebb77ba508ead270b197e0fe35a01eab
kaistullich <kairstullich@gmail.com>
2017-02-01 10:43:10 -0800

    moved try/except block outside of loop, and created first entry into DB

--- currency.py
+++ currency.py
@@ -16,23 +16,25 @@ def index():
 
     ''' euro_currency[0] = Euro
         euro_currency[1] = 1.00 USD -> 1.00 EUR
-        euro_currency[2] = 1.00 EUR -> 1.00 USD '''
+        euro_currency[2] = 1.00 EUR -> 1.00 USD 
+    '''
     euro_currency = []
-    for tag in currency_table_row[:3]:
+    for tag in currency_table_row[1:3]:
         euro_text = tag.get_text()  # pull only the text from the 'td' tags
         euro_append = euro_currency.append(euro_text)  # append the text
 
-        try:
-            conn = sqlite3.connect('current_currency.sqlite')
-            c = conn.cursor()  # cursor
-            c.execute('CREATE TABLE IF NOT EXISTS currency (current_conversion TEXT)')
-            c.execute('INSERT INTO currency VALUES (?)''', (name,))
-            conn.commit()
-            conn.close()
-            
-        except Exception as e:
-            pass
+    try:
+        conn = sqlite3.connect('current_currency.sqlite')
+        c = conn.cursor()  # cursor
+        # c.execute('''CREATE TABLE currency 
+        #             (usd_to_eur real, eur_to_usd real)''')
+        c.execute('INSERT INTO currency VALUES (?, ?)', (euro_currency[0], euro_currency[1]))
+        conn.commit()
+        conn.close()
 
+    except Exception as e:
+        print(e)
+            
     return render_template('currency.html', euro_currency=euro_currency)
     
 if __name__ == '__main__':
